11803c43ebe18c76e82a6dc6e7a4bc0a085e49dc
HomePC <HomePC@PC>
2024-02-13 21:37:57 +0300

    Add project files.

---
+++ WinFormsApp2/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Object_Demonstration.Show()
+
+
+    End Sub
+
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles MyBase.Click
+        Me.WindowState = FormWindowState.Maximized
+        Me.BackColor = Color.Red
+
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ WinFormsApp2/Object Demonstration.vb
@@ -0,0 +1,9 @@
+﻿Public Class Object_Demonstration
+    Private Sub RadioButton5_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton5.CheckedChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
